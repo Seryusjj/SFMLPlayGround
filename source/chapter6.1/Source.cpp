60b9a3ba483e84a65761a425bb498171c64fa842
Sergio Jimenez <sergiojj932@gmail.com>
2020-04-07 23:33:32 +0200

    First opengl example

---
+++ source/chapter6.1/Source.cpp
@@ -0,0 +1,26 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void renderScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0, 0.0, 0.0, 1.0);//clear yellow
+	// Draw game objects here
+}
+
+int main(int argc, char** argv)
+{
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(800, 600, " Hello OpenGL ", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+	while (!glfwWindowShouldClose(window)) {
+		// render our scene
+		renderScene();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+	return 0;
+}
