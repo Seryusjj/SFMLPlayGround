d5479f42676af6e75c4ef86a5e07e35a59f2b33e
Sergio Jimenez <sergiojj932@gmail.com>
2020-04-07 21:44:21 +0200

    Add chapter 4 code

--- source/chapter3/Source.cpp
+++ source/chapter3/Source.cpp
@@ -1,5 +1,4 @@
-#include "SFML\Graphics.hpp"
-
+#include <SFML\Graphics.hpp>
 
 sf::Vector2f viewSize(1024, 768);
 sf::VideoMode vm(viewSize.x, viewSize.y);
@@ -19,8 +18,6 @@ sf::Sprite heroSprite;
 
 
 void init() {
-
-
 	skyTexture.loadFromFile("Assets/graphics/sky.png");
 	skySprite.setTexture(skyTexture);
 
@@ -31,89 +28,65 @@ void init() {
 	heroSprite.setTexture(heroTexture);
 	heroSprite.setPosition(sf::Vector2f(viewSize.x / 2, viewSize.y / 2));
 	heroSprite.setOrigin(heroTexture.getSize().x / 2, heroTexture.getSize().y / 2);
-
-
 }
 
 
-void updateInput() {
-
-	sf::Event event;
-
+void updateInput(sf::Event& event) {
 	// while there are pending events...
 	while (window.pollEvent(event)) {
 
 		//printf(" polling events \n");
-
 		if (event.type == sf::Event::KeyPressed) {
-
 			if (event.key.code == sf::Keyboard::Right) {
-
 				playerMoving = true;
 			}
 		}
 
 		if (event.type == sf::Event::KeyReleased) {
-
 			if (event.key.code == sf::Keyboard::Right) {
-
 				playerMoving = false;
 			}
-
 		}
 
 
 		if (event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed)
 			window.close();
-
-
 	}
-
 }
 
 
 void update(float dt) {
-
-
 	if (playerMoving) {
-
 		heroSprite.move(50.0f * dt, 0);
 	}
-
 }
 
 void draw() {
-
-
 	window.draw(skySprite);
 	window.draw(bgSprite);
-
 	window.draw(heroSprite);
-
 }
 
 
 
 int main() {
-
-	sf::Clock clock;
-
+	
 	init();
 
-	while (window.isOpen()) {
+	sf::Clock clock;
+	sf::Event _event;
 
-		updateInput();
+	while (window.isOpen()) {
+		updateInput(_event);
 
 		sf::Time dt = clock.restart();
 		update(dt.asSeconds());
-
 
 		window.clear(sf::Color::Red);
 
 		draw();
 
 		window.display();
-
 	}
 
 	return 0;
