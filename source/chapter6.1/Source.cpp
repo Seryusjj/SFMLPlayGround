921261a7c78d15f458774a0987d7f1fea4867a32
Sergio Jimenez <sergiojj932@gmail.com>
2020-04-08 14:39:29 +0200

    Chapter 6 and glm

--- source/chapter6.1/Source.cpp
+++ source/chapter6.1/Source.cpp
@@ -3,7 +3,7 @@
 
 void renderScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0);//clear yellow
+	glClearColor(255.0, 255.0, 0.0, 1.0);//clear yellow
 	// Draw game objects here
 }
 
